cf7eabf77213bb49add836b3ce3ebf55c04e9bc8
黃敏修 <>
2023-11-09 16:38:49 +0800

    no message

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <componetsA />
+  <div class="mainWrap">
+    <div class="sublist">
+      <div>
+        <componetsB>Skill</componetsB>
+        <componetsC />
+        <componetsD />
+      </div>
+
+
+    </div>
+    <div class="sublist">
+      <div>
+        <componetsB>about me</componetsB>
+        <p>嗨你好，我是 Gary，來自桃園市，畢業於中山大學，目標是成為一名厲害的軟體工程師。</p>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+import componetsA from "./components/componetsA.vue"
+import componetsB from "./components/componetsB.vue"
+import componetsC from "./components/componetsC.vue"
+import componetsD from "./components/componetsD.vue"
+
+</script>
+
+<style lang="scss" scoped>
+.mainWrap {
+  display: flex;
+
+  .sublist {
+    width: 50%;
+
+    div {
+      p {
+        margin-left: 5%;
+      }
+    }
+  }
+}
+</style>
